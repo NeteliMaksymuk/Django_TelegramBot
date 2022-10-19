e9eafd1aa6aaea8a52ce58c090092d57b5650f00
Neteli Maksymuk <maksymuknatalia@gmail.com>
2022-10-19 18:27:55 +0300

    Add models

--- backend/src/django_telegram_bot/recipes/models.py
+++ backend/src/django_telegram_bot/recipes/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    GENDER = [
+        ('m', 'male'),
+        ('f', 'female'),
+        ('c', 'co-ed'),
+    ]
+    first_name = models.CharField(
+        verbose_name='first_name',
+        max_length=48
+    )
+    last_name = models.CharField(
+        verbose_name='last_name',
+        max_length=48
+    )
+    telegram_username = models.CharField(
+        verbose_name='telegram_username',
+        max_length=48
+    )
+    name = models.CharField(
+        verbose_name='name',
+        max_length=48
+    )
+    gender = models.CharField(
+        max_length=6,
+        verbose_name='gender',
+        choices=GENDER
+    )
+
+    class Meta:
+        verbose_name = 'Profile'
+
+
+class Recipes(models.Model):
+
+    recipe_name = models.CharField(
+        verbose_name='recipe_name',
+        max_length=48
+    )
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Recipes'
+
