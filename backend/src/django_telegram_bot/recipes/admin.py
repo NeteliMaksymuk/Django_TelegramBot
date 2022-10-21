0401e6e5accd34c83739b5220134fefcbb6e2fd7
Neteli Maksymuk <maksymuknatalia@gmail.com>
2022-10-21 20:29:01 +0300

    Complete simple bot

--- backend/src/django_telegram_bot/recipes/admin.py
+++ backend/src/django_telegram_bot/recipes/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Recipes, Profile
 # Register your models here.
+admin.site.register(Recipes)
+admin.site.register(Profile)
--- backend/src/django_telegram_bot/recipes/management/commands/bot.py
+++ backend/src/django_telegram_bot/recipes/management/commands/bot.py
@@ -1,5 +1,3 @@
-import os
-
 import telebot
 from telebot import types
 
@@ -10,11 +8,8 @@ from django_telegram_bot.settings import TOKEN
 
 bot = telebot.TeleBot(TOKEN)
 
-# початок якщо юзер(message.from_user.username)
-# є в базі то в менюшку якщо не то робить профіль
-# профіль зберігається
-
 user_dict = {}
+
 
 class User:
     def __init__(self, name):
@@ -25,26 +20,26 @@ class User:
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     chat_id = message.chat.id
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add('Yes', 'No')
     msg = bot.reply_to(message, """\
-                                    Hi there.Do you have a account?\n No/Yes                                
-                                """)
+                                    Hi there.Do you have a account?\n                                 
+                                """, reply_markup=markup)
     bot.register_next_step_handler(msg, test)
-    # bot.send_message(chat_id, msg)
-    # if not Profile.objects.filter(telegram_username=message.from_user.username).exists():
-    #     msg = bot.send_message(chat_id, """\
-    #     What's your name?
-    #     """)
-    #     bot.register_next_step_handler(msg, process_name_step)
 
-def test (message):
+
+def test(message):
     chat_id = message.chat.id
-    if message.text=='No':
+    if message.text == 'No':
         msg = bot.reply_to(message, """\
-                                            Name?                                   
+                                            What is your name?                                  
                                         """)
         bot.register_next_step_handler(msg, process_name_step)
-    b = Profile.objects.get(telegram_username=message.from_user.username)
-    bot.send_message(chat_id, 'you \n' + b.name + '\n Sex:' + b.gender)
+    elif message.text == 'Yes':
+        bot.send_message(chat_id, 'if you want to open menu send me \n/menu')
+    else:
+        print('ops')
+
 
 def process_name_step(message):
     try:
@@ -53,11 +48,12 @@ def process_name_step(message):
         user = User(name)
         user_dict[chat_id] = user
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        markup.add('Male', 'Female','Co-ed')
+        markup.add('Male', 'Female', 'Co-ed')
         msg = bot.reply_to(message, 'What is your gender', reply_markup=markup)
         bot.register_next_step_handler(msg, process_sex_step)
     except Exception as e:
         bot.reply_to(message, 'oooops')
+
 
 def process_sex_step(message):
     try:
@@ -69,38 +65,42 @@ def process_sex_step(message):
         else:
             raise Exception("Unknown sex")
 
-        types.ReplyKeyboardRemove(selective=False)# видаляє клаву після використання
+        types.ReplyKeyboardRemove(selective=False)
         msg = bot.send_message(chat_id, 'Nice to meet you \n' + user.name + '\n Sex:' + user.sex)
         profile = Profile(name=user.name, last_name=message.from_user.last_name,
                           first_name=message.from_user.first_name, gender=user.sex,
                           telegram_username=message.from_user.username)
         profile.save()
+        bot.send_message(chat_id, 'Send something to check if you are not a bot')
         bot.register_next_step_handler(msg, menu_bar)
     except Exception as e:
-        bot.reply_to(message, 'oooops')
+        bot.reply_to(message, 'OMG,you are already exist.\n send me to open menu \n /menu  ')
+
 
 @bot.message_handler(commands=['menu'])
 def menu_bar(message):
     chat_id = message.chat.id
-
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
     markup.add('About me', 'Recipes')
     msg = bot.reply_to(message, 'What is your next step', reply_markup=markup)
-    bot.register_next_step_handler(msg,next_step)
+    bot.register_next_step_handler(msg, next_step)
 
 
 def next_step(message):
     chat_id = message.chat.id
     if message.text == 'About me':
 
-        b=Profile.objects.get(telegram_username=message.from_user.username)
-        bot.send_message( chat_id,'you \n' + b.name + '\n Sex:' + b.gender)
+        b = Profile.objects.get(telegram_username=message.from_user.username)
+        bot.send_message(chat_id,
+                         '\nYour name: \n' + ' ' + str(b.name) +
+                         '\nYour gender:\n' + ' ' + str(b.gender) +
+                         '\nYour id:\n' + ' ' + str(b.id))
     elif message.text == 'Recipes':
-        res=list()
-        r=Recipes.objects.count()
+        res = list()
+        r = Recipes.objects.count()
         markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
         for i in range(r):
-            print(Recipes.objects.all()[i])
+
             res.append(Recipes.objects.all()[i].recipe_name)
             markup.add(Recipes.objects.all()[i].recipe_name)
         print(res)
@@ -108,11 +108,12 @@ def next_step(message):
         msg = bot.reply_to(message, 'What is your next step', reply_markup=markup)
         bot.register_next_step_handler(msg, next_step2)
 
+
 def next_step2(message):
     chat_id = message.chat.id
     b = Recipes.objects.get(recipe_name=message.text)
-    bot.send_message(chat_id, 'you \n' + b.recipe_name + '\n Sex:' + b.description)
-
+    bot.send_message(chat_id, '\nRecipe name\n' + str(b.recipe_name) +
+                     '\nDescription:\n' + str(b.description))
 
 
 bot.infinity_polling()
